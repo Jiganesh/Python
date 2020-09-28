01121fcbc2b06074e5c0c6ae2402275928bf769e
Jiganesh Patil <67581447+Jiganesh@users.noreply.github.com>
2020-09-28 22:07:01 +0530

    Add files via upload

---
+++ Important Concepts/Decorator.py
@@ -0,0 +1,16 @@
+#Destructor
+
+def div(a,b):
+          print(a/b)
+
+def smart_div(func):
+
+          def inner(a,b):
+                    if b>a:
+                              a,b=b,a
+                    return func(a,b)
+          
+          return inner
+
+div=smart_div(div)
+div(5,7)
