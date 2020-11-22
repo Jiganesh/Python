e0e3ea8145823d7f818e6dbba13b778dff0b6f0d
Jiganesh <jiganeshpatil01071999@gmail.com>
2020-11-23 04:40:17 +0530

    removeElement solved

---
+++ LeetCode Challenge/15_removeElement.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i=0
+        n=len(nums)
+        
+        while i <n:
+            if nums[i]== val:
+                nums[i]=nums[n-1]
+                n-=1
+            else:
+                i+=1
+        return n
