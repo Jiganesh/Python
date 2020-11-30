2d920ae2595f5c7ed223853a692089144127fc87
Jiganesh <jiganeshpatil01071999@gmail.com>
2020-11-30 08:00:25 +0530

    Refactored

--- Object Oriented Programming/Encapsulation.py
+++ Object Oriented Programming/Encapsulation.py
@@ -11,29 +11,29 @@ Instead, it relies on the convention: a class variable that should not directly 
 '''
 
 
-
 class Robot:
 
-          def __init__(self):
-                    print("In class Robot")
-                    self.robot=123
-                    self._robot1=124
-                    self.__robot2=125
-                    
-          def get__robot(self):
-                    return self.__robot2
+    def __init__(self):
+        print("In class Robot")
+        self.robot = 123
+        self._robot1 = 124
+        self.__robot2 = 125
 
-          def set__robot(self,number):
-                    self.__robot2 = number
+    def get__robot(self):
+        return self.__robot2
 
-obj= Robot()
+    def set__robot(self, number):
+        self.__robot2 = number
+
+
+obj = Robot()
 print(obj.robot)
 print(obj._robot1)
 print(obj.get__robot())
 
 obj.set__robot(130)
 print(obj.get__robot())
-#print(obj.__robot2) : This will result in an atribute error
+# print(obj.__robot2) : This will result in an atribute error
 
 '''
 A single underscore: Private Variable, it should not be accessed directly. But nothing stops you from doing that (except convention)
--- Object Oriented Programming/Inheritance.py
+++ Object Oriented Programming/Inheritance.py
@@ -2,36 +2,37 @@
 
 class A:
 
-          def feature1(self):
-                    print("Feature 1 Working")
+    def feature1(self):
+        print("Feature 1 Working")
 
-          def feature2 (self):
-                    print("Feature 2 Working")
+    def feature2(self):
+        print("Feature 2 Working")
+
 
 class B(A):
 
-          def feature3(self):
-                    print("Feature 3 Working")
+    def feature3(self):
+        print("Feature 3 Working")
 
-          def feature4 (self):
-                    print("Feature 4 Working")
+    def feature4(self):
+        print("Feature 4 Working")
 
 
 class C(B):
 
-          def feature5(self):
-                    print("Feature 5 Working")
+    def feature5(self):
+        print("Feature 5 Working")
 
 
-splitter="==================="
-                    
+splitter = "==================="
+
 a = A()
 a.feature1()
 a.feature2()
 
 print(splitter)
 
-b=B()
+b = B()
 b.feature1()
 b.feature2()
 b.feature3()
@@ -39,7 +40,7 @@ b.feature4()
 
 print(splitter)
 
-c=C()
+c = C()
 c.feature1()
 c.feature2()
 c.feature3()
