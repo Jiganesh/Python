3ff51426ab5b824e5dc3d6153ab6f404a176c4ac
Jiganesh <jiganeshpatil01071999@gmail.com>
2020-12-01 14:46:38 +0530

    Refactored

--- Graphs/DFS.py
+++ Graphs/DFS.py
@@ -1,24 +1,26 @@
-d= {
+d = {
 
-"A":["B","D"],
-"B":["A","C"],
-"C":["B"],
-"D":["A","E","F"],
-"E":["D","F","G"],
-"F":["D","E","H"],
-"G":["E","H"],
-"H":["F","G"]
+    "A": ["B", "D"],
+    "B": ["A", "C"],
+    "C": ["B"],
+    "D": ["A", "E", "F"],
+    "E": ["D", "F", "G"],
+    "F": ["D", "E", "H"],
+    "G": ["E", "H"],
+    "H": ["F", "G"]
 
-          }
+}
+
 
 def dfs(visited, graph, node):
-          if node not in visited:
-                    visited.append(node)
-                    for neighbour in graph[node]:
-                              dfs(visited,graph, neighbour)
-          return visited
+    if node not in visited:
+        visited.append(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+    return visited
 
-print(dfs([],d,"A"))
+
+print(dfs([], d, "A"))
 
 
 '''
