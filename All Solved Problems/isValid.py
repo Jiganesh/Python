3b3ab6b2a2194d305c0db1a7fe32bf4cce289545
Jiganesh <jiganeshpatil01071999@gmail.com>
2020-12-05 07:25:58 +0530

    Better Solution

--- All Solved Problems/isValid.py
+++ All Solved Problems/isValid.py
@@ -1,18 +1,17 @@
-#https://leetcode.com/problems/valid-parentheses/
-
+# https://leetcode.com/problems/valid-parentheses/
 class Solution(object):
     def isValid(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        lookup_dictionary = dict(zip(list(")}]"),list("({[")))
-        stack =[]
-        
+        lookup_dictionary = dict(zip(list(")}]"), list("({[")))
+        stack = []
+
         for i in s:
-            if stack and (i in list(lookup_dictionary.keys())) and stack[-1]==lookup_dictionary[i]:
+            if stack and i in ")}]" and stack[-1] == lookup_dictionary[i]:
                 stack.pop()
             else:
                 stack.append(i)
-                
-        return True if len(stack)==0 else False
+
+        return True if len(stack) == 0 else False
