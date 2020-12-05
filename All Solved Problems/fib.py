321226dc2724ca3bd6b0a15ee06754dd07606021
Jiganesh <jiganeshpatil01071999@gmail.com>
2020-12-06 01:04:45 +0530

    leetcode problem solved

---
+++ All Solved Problems/fib.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def fib(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        a, b = 0, 1
+        for _ in range(N):
+            a, b = b, a+b
+        return a
