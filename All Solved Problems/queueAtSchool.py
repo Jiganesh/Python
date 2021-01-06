3587e0f92fdbc0534819573ad424434e62934118
Jiganesh <jiganeshpatil01071999@gmail.com>
2021-01-06 21:23:28 +0530

    not yet solved

--- All Solved Problems/queueAtSchool.py
+++ All Solved Problems/queueAtSchool.py
@@ -1,4 +1,11 @@
 m,n = map(int, input().split())
 string = list(input())
 
-print("".join(string))
+while n:
+    for j in range (0,m-1):
+        if string[j] =="B" and string[j+1]=="G":
+            string [j] ,string[j+1]=string[j+1],string[j]
+    n-=1
+print(string)
+
+
